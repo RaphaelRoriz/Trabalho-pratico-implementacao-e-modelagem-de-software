c8b5e5bcc79857d24cf49427b6df2355335c9d52
Gahiji <gdshanauer@outlook.com>
2019-11-19 14:39:43 -0300

    Criado pagina de edição de perfil

--- UFLANews/src/app/app-routing.module.ts
+++ UFLANews/src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ const routes: Routes = [
   { path: 'news', loadChildren: './pages/news/news.module#NewsPageModule' },
   { path: 'details/:id', canActivate: [AuthGuard], loadChildren: './pages/news-detail/news-detail.module#NewsDetailPageModule' },
   //{ path: 'news-detail/:id', loadChildren: './pages/news-detail/news-detail.module#NewsDetailPageModule' },
-  { path: 'cadastro', loadChildren: './pages/cadastro/cadastro.module#CadastroPageModule' }
+  { path: 'cadastro', loadChildren: './pages/cadastro/cadastro.module#CadastroPageModule' },  { path: 'perfil', loadChildren: './pages/perfil/perfil.module#PerfilPageModule' }
+
 ];
 @NgModule({
   imports: [
