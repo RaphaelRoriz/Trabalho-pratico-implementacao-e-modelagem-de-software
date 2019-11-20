05e938235cf443e5baf7200dc5923b1d00648fb9
Gahiji <gdshanauer@outlook.com>
2019-11-20 18:01:37 -0300

    Adicionado função de criação de contas (falta testar)

--- UFLANews/src/app/pages/cadastro/cadastro.page.ts
+++ UFLANews/src/app/pages/cadastro/cadastro.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService} from 'src/app/services/auth.service';
+import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 import { UsuarioModel } from 'src/app/models/usuarios.model';
-import { UserService} from 'src/app/services/usuario.service';
+import { UserService } from 'src/app/services/usuario.service';
+import { ToastService, MessageType } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -10,24 +11,58 @@ import { UserService} from 'src/app/services/usuario.service';
 })
 export class CadastroPage implements OnInit {
 
-  usuario: UsuarioModel = new UsuarioModel();
+  user: UsuarioModel = new UsuarioModel();
   password: string;
   confirmarPassword: string;
 
-  constructor(public authService: AuthService,
-              public userService: UserService
+  constructor(
+    public authService: AuthService,
+    public userService: UserService,
+    public toastService: ToastService
   ) { }
 
   ngOnInit() {
   }
 
-  async save() {
-    const token: any = await this.authService.register(this.usuario.email, this.password);
-    // Isto foi necessário, pois o token ainda não existe e o usuário
-    // precisa ser cadastrado na base de dados da API
-    await this.userService.add(this.usuario, token.access_token);
-    this.usuario = new UsuarioModel();
-    this.password = "";
-    this.confirmarPassword = "";
+  checkForm() {
+    if (
+      this.user.nome == undefined || this.user.nome.trim() == "" ||
+      this.user.email == undefined || this.user.email.trim() == "" ||
+      this.password == undefined || this.password.trim() == "" ||
+      this.confirmarPassword == undefined || this.confirmarPassword.trim() == "") {
+      this.toastService.presentMessage("Por favor, preencha todos os campos do formulário!", MessageType.ERROR);
+      return false;
+    }
+
+    if (this.password != this.confirmarPassword) {
+      this.toastService.presentMessage("As senhas informadas não conferem!", MessageType.ERROR);
+      return false;
+    }
+
+    return true;
   }
+
+  async save() {
+    if (this.checkForm()) {
+      try {
+
+        const token: any = await this.authService.register(this.user.email, this.password);
+
+        // Isto foi necessário, pois o token ainda não existe e o usuário
+        // precisa ser cadastrado na base de dados da API
+        await this.userService.add(this.user, token.access_token);
+
+        this.user = new UsuarioModel();
+        this.password = "";
+        this.confirmarPassword = "";
+
+        this.toastService.presentMessage("Conta criada com sucesso!", MessageType.SUCCESS);
+      } catch (error) {
+        this.toastService.presentMessage("Já existe uma conta cadastrada com esse endereço de email!", MessageType.ERROR);
+      }
+
+    }
+
+  }
+
 }
